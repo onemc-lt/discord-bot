75a427051d87c42bb35040caab708c345a488f23
onemc-lt <onemc.lt@gmail.com>
2026-02-09 19:48:29 +0200

    commands.js

--- commands.js
+++ commands.js
@@ -1,25 +1,30 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
-
-const TOKEN = process.env.TOKEN;
-const CLIENT_ID = "1470419253797720108";
+import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
 const commands = [
   new SlashCommandBuilder()
-    .setName("announce")
-    .setDescription("Siųsti pranešimą su nuotrauka")
-    .addStringOption(o =>
-      o.setName("text").setDescription("Tekstas").setRequired(true))
-    .addStringOption(o =>
-      o.setName("image").setDescription("Nuotraukos URL").setRequired(false)
+    .setName('announce')
+    .setDescription('Paskelbti pranešimą su embed')
+    .addStringOption(opt =>
+      opt.setName('tekstas')
+        .setDescription('Žinutės tekstas')
+        .setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName('paveikslelis')
+        .setDescription('Paveiksliuko URL (nebūtina)')
+        .setRequired(false)
     ),
 
   new SlashCommandBuilder()
-    .setName("mcstatus")
-    .setDescription("Minecraft serverio statusas")
-].map(cmd => cmd.toJSON());
+    .setName('mcstatus')
+    .setDescription('Parodo Minecraft serverio statusą')
+].map(c => c.toJSON());
 
-const rest = new REST({ version: "10" }).setToken(TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
-  .then(() => console.log("✅ Slash komandos užregistruotos"))
-  .catch(console.error);
+await rest.put(
+  Routes.applicationCommands(process.env.1470419253797720108),
+  { body: commands }
+);
+
+console.log('✅ Slash komandos užregistruotos');
