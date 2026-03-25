503387c32a856d4491e6d995bc3062a220e1ddb5
onemc-lt <onemc.lt@gmail.com>
2026-03-25 16:04:09 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,41 +1,27 @@
-import {
-  REST,
-  Routes,
-  SlashCommandBuilder,
-  PermissionsBitField,
-  ChannelType
-} from "discord.js";
+import { REST, Routes, SlashCommandBuilder, PermissionsBitField, ChannelType } from "discord.js";
 
 const commands = [
   new SlashCommandBuilder()
     .setName("announce")
     .setDescription("Paskelbti announcement su embed")
     .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
-    .addChannelOption(opt =>
-      opt
-        .setName("kanalas")
-        .setDescription("Į kurį kanalą siųsti announcement")
-        .addChannelTypes(ChannelType.GuildText)
-        .setRequired(true)
-    )
-    .addStringOption(opt =>
-      opt
-        .setName("title")
-        .setDescription("Announcement pavadinimas")
-        .setRequired(true)
-    )
-    .addStringOption(opt =>
-      opt
-        .setName("tekstas")
-        .setDescription("Announcement tekstas")
-        .setRequired(true)
-    )
-    .addStringOption(opt =>
-      opt
-        .setName("paveikslelis")
-        .setDescription("Paveiksliuko URL (nebūtina)")
-        .setRequired(false)
-    ),
+    .addChannelOption(opt => opt
+      .setName("kanalas")
+      .setDescription("Į kurį kanalą siųsti announcement")
+      .addChannelTypes(ChannelType.GuildText)
+      .setRequired(true))
+    .addStringOption(opt => opt
+      .setName("title")
+      .setDescription("Announcement pavadinimas")
+      .setRequired(true))
+    .addStringOption(opt => opt
+      .setName("tekstas")
+      .setDescription("Announcement tekstas")
+      .setRequired(true))
+    .addStringOption(opt => opt
+      .setName("paveikslelis")
+      .setDescription("Paveiksliuko URL (nebūtina)")
+      .setRequired(false)),
   new SlashCommandBuilder()
     .setName("mcstatus")
     .setDescription("Parodo Minecraft serverio statusą")
